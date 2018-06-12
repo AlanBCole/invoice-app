5894cfcdc478e9b7391c00253595287c7f71515e
Alan <alantuneit@gmail.com>
2018-06-12 21:35:42 +0000

    retactoring webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,35 @@
-module.exports = [
-  {
-    entry: './src/app.scss',
+// Thank you to this blog https://hackernoon.com/webpack-3-quickstarter-configure-webpack-from-scratch-30a6c394038a
+// I basically just copied what the author went through
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './app.js',
     output: {
-      // This is necessary for webpack to compile
-      // But we never use style-bundle.js
-      filename: 'style-bundle.js',
-    },
-watch: true,
-    devServer: {
-      contentBase: './src',
-      compress: true,
-      port: 9000,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'bundle.css',
-              },
-            },
-            { loader: 'extract-loader' },
-            { loader: 'css-loader' },
-            {
-              loader: 'sass-loader',
-              options: {
-                includePaths: ['./node_modules'],
-              }
-            },
-          ]
-        }
-      ]
-    },
-  },
-  {
-    entry: './src/Main.js',
-    devtool: 'inline-source-map',
-    output: {
-      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: './js/bundle.js'
     },
     watch: true,
     devServer: {
-      contentBase: './src',
+      contentBase: path.resolve(__dirname, 'dist'),
+      open: true,
+      stats: 'errors-only',
+      port: 8080,
       compress: true,
-      port: 9000,
+      devtool: 'inline-source-map',
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+    ],
     module: {
       rules: [
         {
           test: /\.js$/,
+          include: /src/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
@@ -61,10 +39,79 @@ watch: true,
             },
           },
         },
+        { test: /\.html$/, use: ['html-loader'] },
       ],
     },
   }
-]
+  
+
+// [
+//   {
+//     entry: './src/app.scss',
+//     output: {
+//       // This is necessary for webpack to compile
+//       // But we never use style-bundle.js
+//       filename: 'style-bundle.js',
+//     },
+//     watch: true,
+//     devServer: {
+//       contentBase: './src',
+//       compress: true,
+//       port: 9000,
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.scss$/,
+//           use: [
+//             {
+//               loader: 'file-loader',
+//               options: {
+//                 name: 'bundle.css',
+//               },
+//             },
+//             { loader: 'extract-loader' },
+//             { loader: 'css-loader' },
+//             {
+//               loader: 'sass-loader',
+//               options: {
+//                 includePaths: ['./node_modules'],
+//               }
+//             },
+//           ]
+//         }
+//       ]
+//     },
+//   },
+//   {
+//     entry: './src/main.js',
+//     devtool: 'inline-source-map',
+//     output: {
+//       filename: 'bundle.js',
+//     },
+//     watch: true,
+//     devServer: {
+//       contentBase: './src',
+//       compress: true,
+//       port: 9000,
+//     },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.js$/,
+    //       exclude: /node_modules/,
+    //       use: {
+    //         loader: 'babel-loader',
+    //         options: {
+    //           presets: ['babel-preset-env'],
+    //           plugins: [require('babel-plugin-transform-object-rest-spread')],
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+//   }
+// ]
 
 // module.exports = [
 //   {
