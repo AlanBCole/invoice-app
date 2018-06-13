95804c11ff02ed51066e8773fa28229921c494fe
Alan <alantuneit@gmail.com>
2018-06-13 00:40:13 +0000

    refactored webpack to work with .js and .scss entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-// Thank you to this blog https://hackernoon.com/webpack-3-quickstarter-configure-webpack-from-scratch-30a6c394038a
-// I basically just copied what the author went through
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: './app.js',
+    entry: {
+      'js/bundle': './app.js',
+      'css/style-bundle': './scss/app.scss'
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: './js/bundle.js'
+      filename: './[name].js'
     },
-    watch: true,
+    // watch: true,
+    devtool: 'inline-source-map',
     devServer: {
       contentBase: path.resolve(__dirname, 'dist'),
       open: true,
       stats: 'errors-only',
       port: 8080,
       compress: true,
-      devtool: 'inline-source-map',
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -39,123 +40,34 @@ module.exports = {
             },
           },
         },
-        { test: /\.html$/, use: ['html-loader'] },
+        {
+          test: /\.scss$/,
+          include: /src/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: './css/bundle.css'
+              }
+            },
+            { 
+              loader: 'extract-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: ['./node_modules']
+              }
+            },
+          ]
+        },
+        { 
+          test: /\.html$/,
+          use: ['html-loader'] 
+        },
       ],
     },
-  }
-  
-
-// [
-//   {
-//     entry: './src/app.scss',
-//     output: {
-//       // This is necessary for webpack to compile
-//       // But we never use style-bundle.js
-//       filename: 'style-bundle.js',
-//     },
-//     watch: true,
-//     devServer: {
-//       contentBase: './src',
-//       compress: true,
-//       port: 9000,
-//     },
-//     module: {
-//       rules: [
-//         {
-//           test: /\.scss$/,
-//           use: [
-//             {
-//               loader: 'file-loader',
-//               options: {
-//                 name: 'bundle.css',
-//               },
-//             },
-//             { loader: 'extract-loader' },
-//             { loader: 'css-loader' },
-//             {
-//               loader: 'sass-loader',
-//               options: {
-//                 includePaths: ['./node_modules'],
-//               }
-//             },
-//           ]
-//         }
-//       ]
-//     },
-//   },
-//   {
-//     entry: './src/main.js',
-//     devtool: 'inline-source-map',
-//     output: {
-//       filename: 'bundle.js',
-//     },
-//     watch: true,
-//     devServer: {
-//       contentBase: './src',
-//       compress: true,
-//       port: 9000,
-//     },
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.js$/,
-    //       exclude: /node_modules/,
-    //       use: {
-    //         loader: 'babel-loader',
-    //         options: {
-    //           presets: ['babel-preset-env'],
-    //           plugins: [require('babel-plugin-transform-object-rest-spread')],
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
-//   }
-// ]
-
-// module.exports = [
-//   {
-//     entry: './app.scss',
-//     output: {
-//       // This is necessary for webpack to compile
-//       // But we never use style-bundle.js
-//       filename: 'style-bundle.js',
-//     },
-//     module: {
-//       rules: [{
-//         test: /\.scss$/,
-//         use: [
-//           {
-//             loader: 'file-loader',
-//             options: {
-//               name: 'bundle.css',
-//             },
-//           },
-//           { loader: 'extract-loader' },
-//           { loader: 'css-loader' },
-//           {
-//             loader: 'sass-loader',
-//             options: {
-//               includePaths: ['./node_modules'],
-//             }
-//           },
-//         ]
-//       }]
-//     },
-//   },
-//   {
-//     entry: "./src/Main.js",
-//     output: {
-//       filename: "bundle.js"
-//     },
-//     module: {
-//       loaders: [{
-//         test: /\.js$/,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['env']
-//         }
-//       }]
-//     },
-//   }
-// ];
+  };
